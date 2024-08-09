684454dc122a39ba3c17a03d932a612fce70a93e
aya <164530396+ay4ssin@users.noreply.github.com>
2024-08-09 03:41:27 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,20 +2,58 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Box, Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Divider, Stack, TextField, Typography } from '@mui/material';
 import { useState, useRef, useEffect } from 'react';
+import { useRouter } from 'next/navigation'
 import theme from "./theme";
+import { transform } from "next/dist/build/swc";
+//import { firestore } from "./src/firebase";
+
+
+
+// style for sign up and sign in modal
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%,-50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p:4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap:3,
+};
+
+
 
 export default function Home() {
+
+  // SIGN IN / SIGN UP
+
+  const [open, setOpen] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+
+
+  // MESSAGING
 
   const [history, setHistory] = useState([
     {
       role: "user",
-      parts: [{ text: "👋" }]
+      parts: [{ text: "" }]
     },
     {
       role: "model",
-      parts: [{ text: "Hi! I'm the Headstarter support assistant. How can I help you today?" }]
+      parts: [{ text: "Meow! I'm your PawsitiveCare virtual assistant, here to pounce on any questions you have. How can I help you today?" }]
     }
 
   ]);
@@ -24,12 +62,15 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
 
   const sendMessage = async () => {
+    
     setHistory((history) => [...history, { role: "user", parts: [{ text: message }] }]);
     setMessage('');
 
     const response = await fetch("/api/chat", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { 
+        "Content-Type": "application/json" 
+      },
       body: JSON.stringify([...history, { role: "user", parts: [{ text: message }] }])
     });
 
@@ -55,6 +96,8 @@ export default function Home() {
     scrollToBottom();
   }, [history]);
 
+  // DISPLAY
+
   return (
     <Box
       width="100vw"
@@ -64,6 +107,7 @@ export default function Home() {
       justifyContent="center"
       alignItems="center"
     >
+      
       <Stack
         direction={"column"}
         width="500px"
@@ -72,6 +116,30 @@ export default function Home() {
         p={2}
         spacing={3}
       >
+        <Box
+        fullwidth
+        display='flex'
+        flexDirection='column'
+        justifyContent="center"
+        alignItems="center"
+        >
+        <Box component="img"
+        src="https://i.pinimg.com/564x/b8/51/78/b85178bffa0f26892173b37fe22fec1b.jpg"
+        sx={{borderRadius: '50px',
+          justifyContent:"center",
+          alignItems: 'center',
+          height: 80,
+          width: 80,
+          mt:1
+        }}/>
+        <Typography sx={{mt:1, mb:-1}}>Kitty</Typography>
+        </Box>
+
+        <Divider 
+        fullwidth
+        orientation="horizontal"
+        sx={{my:0}}
+        />
         <Stack
           direction={'column'}
           spacing={2}
@@ -79,7 +147,7 @@ export default function Home() {
           overflow="auto"
           maxHeight="100%"
         >
-          {history.map((message, index) => (
+          {history.slice(1).map((message, index) => (
             <Box
               key={index}
               display="flex"
